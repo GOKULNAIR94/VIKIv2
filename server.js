a51ed356f67bd2799e16231be53d28673a49e9d5
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-06-14 18:59:41 +0530

    Undo

--- server.js
+++ server.js
@@ -22,7 +22,7 @@ restService.post('/inputmsg', function(req, res) {
     console.log( "intentName : " + intentName);
     try
     {
-        if( intentName == 'News' || intentName == 'News - link' || intentName == 'Budget - news'){
+        if( intentName == 'News' || intentName == 'News - link' ){
             varHost = 'vikinews.herokuapp.com';
             varPath = '/inputmsg'; 
         }
