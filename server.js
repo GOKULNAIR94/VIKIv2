4ae5a52904b5bbe532e2818690fec3bbf05e0163
Gokul Nair <gokulgnair94@gmail.com>
2017-06-20 23:34:11 +0530

    Login

--- server.js
+++ server.js
@@ -10,172 +10,65 @@ restService.use(bodyParser.urlencoded({
 }));
 restService.use(bodyParser.json());
 
-var Index = require( "./index" );
-var Login = require( "./loginOSC" );
+var Index = require("./index");
+var Login = require("./loginOSC");
 
 var fs = require('fs');
 
 
 
 
-
 restService.post('/inputmsg', function(req, res) {
 
-  var sessionId = req.body.sessionId;
-  console.log( "sessionId : " + sessionId);
-  var content;
+    var sessionId = req.body.sessionId;
+    console.log("sessionId : " + sessionId);
+    var content;
 
-	content = fs.readFileSync('login.json', 'utf8');
-	console.log( "Content : " + content);
-	content = JSON.parse(content);
+    content = fs.readFileSync('login.json', 'utf8');
+    console.log("Content : " + content);
+    content = JSON.parse(content);
 
-	console.log("Content :" + JSON.stringify(content.items) );
-
-	var intentName = req.body.result.metadata.intentName;
-    console.log( "intentName : " + intentName );
-   	
-   	if( content.items.OSC[sessionId] != null ){
-		var username = content.items.OSC[sessionId].username;
-		var password = content.items.OSC[sessionId].password;
-		console.log( "username : " + username);
-		console.log( "password : " + password);
-
-		Index( req, res, function( result ) {
-            console.log("Index Called");
-        });
-	}
-	else{
-		if( req.body.result.metadata.intentName == "Login" ){
-			console.log("Login Intent");
-	        // var username = req.body.result.parameters['username'];
-    	    // var password = req.body.result.parameters['password'];
-
-    	    Login( req, res, function( result ) {
-	            console.log("Login Called");
-	        });
-		}
-		else{
-			if( intentName == "Default Welcome Intent" ){
-				speech = "Hi There! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help! Please Login. Try saying: I am Gokul and password is Gokul123";
-			}
-			else{
-				speech = "Hi There! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help!";  
-			}
-			return res.json({
-              speech: speech,
-              displayText: speech
-            })
-		}
-		// speech = "I will need your Sales Cloud Credentials. Try saying: I am Gokul and password is Oracle 123";
-		// return res.json({
-		// 	speech: speech,
-		// 	displayText: speech
-		// })
-	}
+    console.log("Content :" + JSON.stringify(content.items));
 
     var intentName = req.body.result.metadata.intentName;
-    console.log( "intentName : " + intentName );
-	if( intentName == "Default Welcome Intent"){
-		try
-		{
-			http.get("https://vikinews.herokuapp.com");
-			http.get("https://vikiviki.herokuapp.com");
-			http.get("https://salty-tor-67194.herokuapp.com");
-			http.get("https://opty.herokuapp.com");
-			speech = "Hi There! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help!";
-                return res.json({
-                  speech: speech,
-                  displayText: speech
-                })
-		}
-		catch(e)
-		{
-			console.log( "Error : " + e );
-		}
-	}
-    var content;
-    var speech = '';
-    var varHost = '';
-    var varPath = '';
-    
-    console.log( "intentName : " + intentName);
-    try
-    {
-        if( intentName == 'News' || intentName == 'News - link' ){
-            varHost = 'vikinews.herokuapp.com';
-            varPath = '/inputmsg'; 
-        }
-		
-		if( intentName == 'Budget' || intentName == 'Expense' ){
-            varHost = 'vikiviki.herokuapp.com';
-            varPath = '/inputmsg'; 
-        }
-        
-        if( intentName == 'reporting' ){
-            varHost = 'salty-tor-67194.herokuapp.com';
-            varPath = '/report';
-        }
-        
-        if( intentName == 'oppty' || intentName=='oppty - next' || intentName=='oppty - custom' || intentName=='oppty - News'  ){
-            //varHost = 'polar-sea-99105.herokuapp.com';
-            varHost = 'opty.herokuapp.com';
-			varPath = '/oppty';
-        }
-        console.log( "varHost : " + varHost );
-        console.log( "varPath : " + varPath);
-        var newoptions = {
-          host: varHost,
-          path: varPath,
-          data: req.body,
-          method:'POST',
-          headers: {
-            'Content-Type': 'application/json'
-          }
-        };
+    console.log("intentName : " + intentName);
 
-        var body = "";
-        var responseObject;
+    if (content.items.OSC[sessionId] != null) {
+        var username = content.items.OSC[sessionId].username;
+        var password = content.items.OSC[sessionId].password;
+        console.log("username : " + username);
+        console.log("password : " + password);
 
-        var post_req = http.request(newoptions, function(response) {
-          response.on('data', function (chunk) {
-            body += chunk;
-          });
-
-          response.on('end', function() {
-              console.log( "Body : " + body );
-              try
-              {
-                responseObject = JSON.parse(body);
-                speech = responseObject.speech;
-                return res.json({
-                  speech: speech,
-                  displayText: speech
-                })
-              }
-              catch(e){
-                speech = "Error occured!";
-                return res.json({
-                  speech: speech,
-                  displayText: speech
-                })
-              }
-          })
-        }).on('error', function(e){
-          speech = "Error occured!";
-            return res.json({
-              speech: speech,
-              displayText: speech
-            })
+        Index(req, res, function(result) {
+            console.log("Index Called");
         });
-        post_req.write(JSON.stringify(req.body));
-        post_req.end();
-      
+    } else {
+        if (req.body.result.metadata.intentName == "Login") {
+            console.log("Login Intent");
+            // var username = req.body.result.parameters['username'];
+            // var password = req.body.result.parameters['password'];
+
+            Login(req, res, function(result) {
+                console.log("Login Called");
+            });
+        } else {
+            if (intentName == "Default Welcome Intent") {
+                speech = "Hi There! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help! Please Login. Try saying: I am Gokul and password is Gokul123";
+            } else {
+                speech = "Hi There! My name is VIKI (Virtual Interactive Kinetic Intelligence) and I am here to help!";
+            }
+            return res.json({
+                speech: speech,
+                displayText: speech
+            })
+        }
+        // speech = "I will need your Sales Cloud Credentials. Try saying: I am Gokul and password is Oracle 123";
+        // return res.json({
+        //  speech: speech,
+        //  displayText: speech
+        // })
     }
-    catch(e)
-    {
-        console.log("Error : " + e );
-    }
-	
+
 });
 
 
