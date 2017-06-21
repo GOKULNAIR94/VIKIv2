0e0da626b5908102b72f624079f9c1ac525fae0c
GOKULNAIR94 <gokulgnair94@gmail.com>
2017-06-21 13:58:01 +0530

    Test

--- loginOSC.js
+++ loginOSC.js
@@ -34,7 +34,7 @@ module.exports = function(req, res) {
         });
         resx.on('end', function() {
             try{
-                console.log( 'resx : ' + JSON.stringify( resx.body ) );
+                console.log( 'resx : ' + JSON.stringify( resx.data ) );
                 var resObj = JSON.parse(responseString);
                 var jsonMap = {
                     "username" : username,
